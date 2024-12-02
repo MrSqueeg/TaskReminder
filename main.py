efc391c9a3613446892add7abef19c46c1ffcdaf
MrSqueeg <thagoglia@gmail.com>
2024-12-01 22:53:50 -0800

    Basic Menu

---
+++ main.py
@@ -0,0 +1,11 @@
+import menu
+
+# Window
+menu = menu.app_menu()
+
+def main():
+    menu.working_menu()
+    menu.window.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ menu.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+import time
+
+class app_menu():
+    # Menu initial setup
+    window = tk.Tk()
+    window.title('Window')
+    width = window.winfo_screenwidth()               
+    height = window.winfo_screenheight()               
+    window.geometry("%dx%d" % (width, height))
+    popup_time = 5
+    
+    def reset_window(self):
+        for widget in self.window.winfo_children():
+            widget.pack_forget()
+
+    def home_menu(self):
+        self.reset_window()
+
+        # Title
+        title_label = ttk.Label(master = self.window, text = 'Test label', font = 'Calibri 24 bold')
+        title_label.pack()
+
+        frame = ttk.Frame(master = self.window)
+        button = ttk.Button(master = frame, text = 'swap menu', command = self.working_menu)
+        button.pack(side = 'left', padx = 10)
+        frame.pack(pady = 10)
+
+    def working_menu(self):
+        self.reset_window()
+
+        title_label = ttk.Label(master = self.window, text = 'Are you working?', font = 'Calibri 24 bold')
+        title_label.pack()
+
+        frame = ttk.Frame(master = self.window)
+        yes_button = ttk.Button(master = frame, text = 'Yes', command = self.working)
+        no_button = ttk.Button(master = frame, text = 'No', command = self.not_working)
+        yes_button.pack(side = 'left', padx = 10)
+        no_button.pack(side = 'left', padx = 10)
+        frame.pack(pady = 10)
+
+    def not_working(self):
+        self.home_menu()
+
+    def working(self):
+        self.window.withdraw()
+        time.sleep(self.popup_time)
+        self.window.deiconify()
+    
+    def quit(self):
+        print('quitting')
+        self.window.destroy()
